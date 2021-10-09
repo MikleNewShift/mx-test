7af0426ff3f782ab3cfa330e0b42d35eaf5e0b4b
Mikhail <barulin.ma@gmail.com>
2021-10-09 18:42:13 +0300

    update db.json + create questions generator

--- js/index.js
+++ js/index.js
@@ -1,7 +1,28 @@
-function getData() {
-  fetch("https://my-json-server.typicode.com/MikleNewShift/mx-test/db/0")
+"use strict";
+
+async function showData() {
+  let data;
+
+  await fetch(
+    "https://my-json-server.typicode.com/MikleNewShift/mx-test/questions"
+  )
     .then((response) => response.json())
-    .then((json) => console.log(json));
+    .then((json) => {
+      data = json;
+    });
+
+  console.log(data);
+
+  const questions = data;
+
+  const container = document.querySelector(".container");
+
+  questions.map((question) => {
+    container.innerHTML += `<h1>${question.title}</h1>`;
+    question.answers.map((answer) => {
+      container.innerHTML += `<p>${answer.name}</p>`;
+    });
+  });
 }
 
-getData();
+showData();
